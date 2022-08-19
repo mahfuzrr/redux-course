429fa1fa7453d585bef0d6de274d2b3bb17eef27
Mahfuzur Rahman <51076032+Mahfuzz11@users.noreply.github.com>
2022-08-20 00:03:36 +0600

    applyMiddleware

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,16 @@
-import {createStore} from 'redux';
+import {applyMiddleware, createStore} from 'redux';
 import rootReducers from './rootReducers';
 
-const store = createStore(rootReducers);
+const MyLogger = (store) => (next) => (action) =>{
+    console.log(`Action: ${JSON.stringify(action)}`);
+    console.log(`Before: ${JSON.stringify(store.getState())}`);
+
+    const updateState = [action].reduce(rootReducers, store.getState());
+
+    console.log(`Next state: ${JSON.stringify(updateState)}`);
+    //return next(action);
+}
+
+const store = createStore(rootReducers, applyMiddleware(MyLogger));
 
 export default store;
